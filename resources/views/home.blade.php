5a16de9cc037e9d693f69250ce6c6468f90233f7
Thavi <thavit97@gmail.com>
2020-03-23 12:08:54 +0000

    Installed Bootstrap and ordered the games into cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,40 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.template')
+@section('title', 'GamesCritic')
+@section('body')
+<div class="col-lg-12 text-center">
+  Hello World! <br><br>
+  <h2>Here are the latest games!</h2>
+  @foreach($games as $game)
+  {{$game['game']['name'] . " " . $game['game']['aggregated_rating'] ." " . gmdate('d-m-Y', $game['date'])}} <br>
+  @endforeach
+</div>
 
-        <title>Laravel</title>
+<div class="container container-fluid">
+  <div class="row row-cols-1 row-cols-md-3">
+    @foreach($games as $game)
+    <div class="col mb-4">
+      <!-- Card -->
 
-    </head>
-    <body>
-      Hello World! <br><br>
+      <div class="card h-100">
+        <!--Card image-->
+        <div class="view overlay">
+          {{ HTML::image($game['game']['artworks'][0], 'Card image cap', array('class' => 'card-img-top')) }}
+          <a href="#!">
+            <div class="mask rgba-white-slight"></div>
+          </a>
+        </div>
 
-      <h2>Here are the latest games!</h2>
-      @foreach($games as $game)
-      {{$game['game']['name'] . " " . $game['game']['aggregated_rating'] ." " . gmdate('d-m-Y', $game['date'])}} <br>
-      @endforeach
-    </body>
-</html>
+        <!--Card content-->
+        <div class="card-body">
+          <!--Title-->
+          <h4 class="card-title">{{$game['game']['name']}}</h4>
+          <!--Text-->
+          <p class="card-text">{{$game['game']['summary']}}</p>
+        </div>
+      </div>
+
+    </div>
+    @endforeach
+  </div>
+</div>
+@endsection
