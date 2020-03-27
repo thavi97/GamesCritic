a16e020983569e6083651089f7904db0b12144ca
Thavi <thavit97@gmail.com>
2020-03-27 14:13:22 +0000

    Implemented Google Recaptcha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
-
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
